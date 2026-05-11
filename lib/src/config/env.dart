5a75ab0e7198158b0d98d1b83ba1ec7a7d88b73e
Tchauperas <marcos.vhynycyus@gmail.com>
2026-05-11 20:22:40 -0300

    final steps

--- lib/src/config/env.dart
+++ lib/src/config/env.dart
@@ -1,6 +1,6 @@
 class Env {
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.3.2:3000',
+    defaultValue: 'https://finansme.onrender.com',
   );
 }
